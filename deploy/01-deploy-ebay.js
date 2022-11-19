ffcf4ee7dfeb6eb04230523ddacb2c0037790497
DulquerSumon <m.dulquer.sumon@gmail.com>
2022-11-19 10:45:25 +0600

    initial commit

---
+++ deploy/01-deploy-ebay.js
@@ -0,0 +1,18 @@
+const { network } = require("hardhat");
+const { verify } = require("../utils/verify");
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const ebay = await deploy("Ebay", {
+    from: deployer,
+    log: true,
+    args: [],
+    waitConfirmations: network.config.blockConfirmations || 1,
+  });
+  if (network.config.chainId == 5 && process.env.ETHERSCAN_API_KEY) {
+    await verify(ebay.address, []);
+  }
+  console.log(`Contract deployed at : ${ebay.address}`);
+};
